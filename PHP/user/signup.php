c1cbded7fe9a3e73853f1f55110b1eb4e5654266
remainedmind <lemoonadresse@gmail.com>
2024-05-13 11:08:40 +0300

    Added password validation

--- PHP/user/signup.php
+++ PHP/user/signup.php
@@ -10,19 +10,19 @@
   <main class="login">
     <div class="login__container">
       <img class="logo" src="../images/logo.png" alt="Logo">
-      <h2 class="login__title">Name</h2>
-      <form class="login__form">
+      <h2 class="login__title">Welcome to The Golden Cup</h2>
+      <form class="login__form" action="<?php echo $_SERVER["PHP_SELF"];  ?>" method="post">
         <fieldset class="login__fieldset">
           <p class="login__input-name">Name</p>
-          <input class="login__input" type="text">
+          <input class="login__input" type="text" name="name">
           <p class="login__input-name">Username</p>
-          <input class="login__input" type="text">
+          <input class="login__input" type="text" name="username">
           <p class="login__input-name">E-mail</p>
-          <input class="login__input" type="email">
+          <input class="login__input" type="email" name="email">
           <p class="login__input-name">Password</p>
-          <input class="login__input" type="password">
+          <input class="login__input" type="password" name="pass">
           <p class="login__input-name">Confirm password</p>
-          <input class="login__input" type="password">
+          <input class="login__input" type="password" name="pass_confirmation">
         </fieldset>
         <div class="login__buttons">
           <button type="reset" class="login__button">Clear</button>
@@ -32,5 +32,43 @@
       </form>
     </div>
   </main>
+
+  <?php
+  $con = mysqli_connect("0.0.0.0","root",null,"CMPR_Project", 4306);
+  if (!$con)
+  {
+      echo "Error in Connection: ";
+      exit();
+  }
+
+
+  if($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+      $name = $_POST["name"];
+      $username = $_POST["username"];
+      $email = $_POST["email"];
+      $password = $_POST["pass"];
+      $pass_confirmation = $_POST["pass_confirmation"];
+
+      if ($password != $pass_confirmation){
+          echo "Both your password inputs must match!";
+          exit();
+
+      }
+
+//      $sql_stmt = $con->prepare("INSERT INTO users(name, username, email, password) VALUES ('$name', '$username', '$email', '$password')";)
+      $sql = "INSERT INTO users(name, username, email, password) VALUES ('$name', '$username', '$email', '$password')";
+
+      if(mysqli_query($con, $sql)){
+          echo "<h3>You've signed up successfully!</h3>";
+          header("Location: homePage.php");
+      }
+      else{
+          echo "Error in adding new user .. ";
+      }
+
+
+  }
+  ?>
 </body>
 </html>
